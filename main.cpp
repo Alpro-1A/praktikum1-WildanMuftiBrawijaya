a0ec173f88c88f3975de9f138e77f9726ca37835
mufti <wildanmuftibrawijaya@gmail.com>
2023-10-14 22:09:44 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------------------------" << endl;
+   cout << "Nama : Wildan Mufti Brawijaya" << endl << endl;
+    cout << "NPM : 2310631170121" << endl << endl;
+    cout << "Kelas : 1A - Informatika" << endl << endl;
+    cout << "Cita-cita : Programmer" << endl << endl;
+    cout << "Motivasi Masuk Informatika : Dari Rekomendasi Di Internet" << endl;
+    cout << "---------------------------------------------------------" << endl;
+
+    return 0;
+}
